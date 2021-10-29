1f9fc0c5f6340370fd0a580b99afb5caa55b0860
snowty-developer <snowty.player@gmail.com>
2021-10-30 02:17:02 +0300

    Initial Commit

---
+++ TestForAEON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestForAEON
+//
+//  Created by Александр Зубарев on 30.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
